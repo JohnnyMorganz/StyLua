3b40e3f2ce097ffd669ac76790b3593235aefd79
JohnnyMorganz <therobloxbot@gmail.com>
2020-12-15 20:06:35 +0000

    Add beginning of library entry

---
+++ luafmt-lib/src/formatters/mod.rs
@@ -0,0 +1,4 @@
+pub mod assignment_formatter;
+pub mod eof_formatter;
+pub mod functions_formatter;
+pub mod value_formatter;
--- luafmt-lib/src/lib.rs
+++ luafmt-lib/src/lib.rs
@@ -1,7 +1,33 @@
+use full_moon::ast::{owned::Owned};
+use full_moon::visitors::VisitorMut;
+
+mod formatters;
+
+pub fn format_code(code: &'static str) -> String {
+    // let ast = match full_moon::parse(&code) {
+    //     Ok(ast) => ast.owned(),
+    //     Err(error) => {
+    //         error!("Error parsing");
+    //     }
+    // };
+
+    let mut ast = full_moon::parse(&code).expect("error parse").owned();
+
+    // We must do these in specific order, so that the latter formattings have less work to do
+    ast = formatters::value_formatter::ValueFormatter::default().visit_ast(ast);
+    ast = formatters::assignment_formatter::AssignmentFormatter::default().visit_ast(ast);
+    ast = formatters::eof_formatter::EofFormatter::default().visit_ast(ast);
+
+    full_moon::print(&ast)
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::format_code;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_complete_format() {
+        let code = "local       x     ,       y = 1   , 'foo'         \nfoo = 1";
+        let output_code = "local x, y = 1, \"foo\"\nfoo = 1\n";
+        assert_eq!(format_code(code), output_code);
     }
 }
