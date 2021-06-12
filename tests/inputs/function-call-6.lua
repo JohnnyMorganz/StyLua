dda86a8d0ac4d03afec2fd00606c67e87e0300b4
JohnnyMorganz <johnnymorganz@outlook.com>
2021-06-12 16:25:11 +0100

    Update tests

---
+++ tests/inputs/function-call-6.lua
@@ -0,0 +1,6 @@
+-- hug table braces with parentheses
+
+print({ foo_variable = "some long value", foo_variable = "some long value", foo_variable = "some long value", foo_variable = "some long value", })
+
+print({ foo_variable = "somenge", foo_variable = "malue", foo_variable = "alueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" })
+
