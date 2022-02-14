145c0433e61cf8e14f5c6a1905ac30dc10b3a19c
JohnnyMorganz <johnnymorganz@outlook.com>
2022-02-14 16:30:40 +0000

    Add test handler

--- tests/tests.rs
+++ tests/tests.rs
@@ -14,6 +14,14 @@ fn test_standard() {
 }
 
 #[test]
+fn test_full_moon_test_suite() {
+    insta::glob!("inputs-full_moon/*.lua", |path| {
+        let contents = std::fs::read_to_string(path).unwrap();
+        insta::assert_snapshot!(format(&contents));
+    })
+}
+
+#[test]
 #[cfg(feature = "luau")]
 fn test_luau() {
     insta::glob!("inputs-luau/*.lua", |path| {
