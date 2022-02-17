d92dace29f637bf9cd880109cdbda4a711e593bb
JohnnyMorganz <johnnymorganz@outlook.com>
2022-02-17 19:03:33 +0000

    Add test case

---
+++ tests/inputs-luau/if-expression-comments-2.lua
@@ -0,0 +1,9 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/374
+context:reportError(("Required input field %s.%s cannot be deprecated."):format(inputObj.name, field.name), {
+	getDeprecatedNode((field :: InputField).astNode),
+	if field.astNode ~= nil
+			-- ROBLOX FUNTIME START: Luau
+			then (field :: any).astNode.type
+			-- ROBLOX FUNTIME END
+			else nil,
+})
