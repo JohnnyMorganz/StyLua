ee99789d50e6b9ae570e852ef0685eb835d2afc0
JohnnyMorganz <johnnymorganz@outlook.com>
2022-02-17 19:10:17 +0000

    Add another test case

--- tests/inputs-luau/if-expression-comments-2.lua
+++ tests/inputs-luau/if-expression-comments-2.lua
@@ -1,3 +1,8 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/375
+local x = if true
+	then foo -- comment
+	else nil
+
 -- https://github.com/JohnnyMorganz/StyLua/issues/374
 context:reportError(("Required input field %s.%s cannot be deprecated."):format(inputObj.name, field.name), {
 	getDeprecatedNode((field :: InputField).astNode),
