aea947f9c0cc5264dab508c2952f384002af6fc2
JohnnyMorganz <johnnymorganz@outlook.com>
2022-03-17 17:23:25 +0000

    Update tests

---
+++ tests/inputs/generic-for-hug.lua
@@ -0,0 +1,12 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/405
+do
+	for _,v in ipairs({
+		Kind.SELECTION_sET,
+		Kind.DIRECTIVE,
+		Kind.OEPRATION_DEFINITION,
+		Kind.INLINE_FRAGMENT,
+		Kind.FRAGMENT_DEFINITION,
+		Kind.ARGUMENT,
+	}) do
+	end
+end
