460eb678f53e7dd4693bb3f31ad2329ff139e841
JohnnyMorganz <johnnymorganz@outlook.com>
2022-03-31 14:21:13 +0100

    Update test case for multiple assigns

--- tests/inputs/hang-parens-assign-no-indent.lua
+++ tests/inputs/hang-parens-assign-no-indent.lua
@@ -30,3 +30,42 @@ local test = (
 	+ long_variable_name
 	+ long_variable_name
 )
+
+-- Multiple assigns
+local test, test2 = (
+	long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+), (
+	long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+)
+
+-- Multiple assigns of different types
+local test, test2 = foo and bar or baz, (
+	long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+)
