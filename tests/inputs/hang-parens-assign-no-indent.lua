55bdd14884b9033b45ea3b664dada1f5e34c3394
JohnnyMorganz <johnnymorganz@outlook.com>
2022-03-31 14:18:51 +0100

    Add test case

---
+++ tests/inputs/hang-parens-assign-no-indent.lua
@@ -0,0 +1,32 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/274
+local tbl = {
+	key = long_variable_name,
+	key = long_variable_name,
+	key = long_variable_name,
+	key = long_variable_name,
+	key = long_variable_name,
+	key = long_variable_name,
+	key = long_variable_name,
+}
+function_call(
+	long_variable_name,
+	long_variable_name,
+	long_variable_name,
+	long_variable_name,
+	long_variable_name,
+	long_variable_name,
+	long_variable_name,
+	long_variable_name
+)
+local test = (
+	long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+	+ long_variable_name
+)
