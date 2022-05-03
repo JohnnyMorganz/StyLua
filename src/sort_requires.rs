620344c2bd40c05c7985b21f9cb1679dec80c95d
JohnnyMorganz <johnnymorganz@outlook.com>
2022-05-03 18:38:28 +0100

    Handle chained dependencies

--- src/sort_requires.rs
+++ src/sort_requires.rs
@@ -13,7 +13,7 @@
 //!     - We then leave one line gap, and include all the requires (in a sorted order)
 //!     - Requires are sectioned by the variables that they rely on
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use full_moon::{
     ast::{
@@ -125,40 +125,66 @@ fn is_require_assignment(assignment: &LocalAssignment) -> bool {
         && parse_require_function(extract_assignment_expression(assignment)).is_some()
 }
 
-fn find_dependency_names(requires: &[&StmtSemicolon]) -> HashSet<String> {
-    let mut set = HashSet::new();
+fn create_map_from_assignments(block: &Block) -> HashMap<String, &StmtSemicolon> {
+    let mut map = HashMap::new();
 
-    for (require, _) in requires {
-        let expression = extract_assignment_expression(extract_local_assignment(require));
-        let argument = match parse_require_function(expression) {
-            Some(FunctionArgs::String(_)) => continue, // A string require cannot have any dependencies
-            Some(FunctionArgs::TableConstructor(_)) => todo!("require of a table"),
-            Some(FunctionArgs::Parentheses { arguments, .. }) => arguments.iter().next().unwrap(),
-            _ => unreachable!(),
-        };
-
-        let dependency = match argument {
-            Expression::Value { value, .. } => {
-                match &**value {
-                    Value::String(_) => continue, // A string require cannot have any dependencies
-                    Value::Var(var) => match var {
-                        Var::Name(token) => name_from_token(token.token_type()),
-                        Var::Expression(var_expression) => match var_expression.prefix() {
-                            Prefix::Name(token) => name_from_token(token.token_type()),
-                            _ => todo!("non-standard require expression"),
-                        },
-                        other => unreachable!("unknown node: {:?}", other),
-                    },
-                    _ => todo!("non-standard require expression"),
+    for stmt_semicolon in block.stmts_with_semicolon() {
+        match &stmt_semicolon.0 {
+            Stmt::LocalAssignment(assignment) => {
+                // TODO: should we map multi assignments?
+                for name in assignment.names() {
+                    map.insert(name_from_token(name.token_type()), stmt_semicolon);
                 }
             }
-            _ => todo!("non-standard require expression"),
-        };
-
-        set.insert(dependency);
+            _ => continue,
+        }
     }
 
-    set
+    map
+}
+
+fn find_dependency_name(stmt: &StmtSemicolon) -> Option<String> {
+    match &stmt.0 {
+        Stmt::LocalAssignment(assignment) => {
+            // TODO: handle multi expression?
+            if let Some(expression) = assignment.expressions().iter().next() {
+                // Handle if its a require function
+                let expression = match parse_require_function(expression) {
+                    Some(FunctionArgs::String(_)) => return None, // A string require cannot have any dependencies
+                    Some(FunctionArgs::TableConstructor(_)) => todo!("require of a table"),
+                    Some(FunctionArgs::Parentheses { arguments, .. }) => {
+                        arguments.iter().next().unwrap()
+                    }
+                    Some(other) => unreachable!("unknown node: {:?}", other),
+                    None => expression,
+                };
+
+                let dependency = match expression {
+                    Expression::Value { value, .. } => {
+                        match &**value {
+                            Value::String(_) => return None, // A string require cannot have any dependencies
+                            Value::Var(var) => match var {
+                                Var::Name(token) => name_from_token(token.token_type()),
+                                Var::Expression(var_expression) => match var_expression.prefix() {
+                                    Prefix::Name(token) => name_from_token(token.token_type()),
+                                    _ => todo!("non-standard dependency"),
+                                },
+                                other => unreachable!("unknown node: {:?}", other),
+                            },
+                            _ => todo!("non-standard dependency"),
+                        }
+                    }
+                    _ => todo!("non-standard dependency"),
+                };
+
+                Some(dependency)
+            } else {
+                None
+            }
+        }
+
+        _ => todo!("non-local assignment dependency"),
+    }
 }
 
 fn is_dependency(stmt: &Stmt, dependency_names: &HashSet<String>) -> bool {
@@ -179,10 +205,30 @@ fn find_requires(
     Vec<&StmtSemicolon>,
     Vec<&StmtSemicolon>,
 ) {
+    let assignment_map = create_map_from_assignments(block);
+
+    // Partition into require statements and the remaining statements
     let (requires, remainders): (Vec<_>, _) = block.stmts_with_semicolon().partition(|stmt| matches!(&stmt.0, Stmt::LocalAssignment(assignment) if is_require_assignment(assignment)));
 
+    // Find all the dependency variables from the list of remaining statements
+    // We need to continuously search the list to find all the statements (i.e., local a = __, local b = a.x, local c = require(b.y) : both a and b are dependencies)
+    let mut dependency_names = HashSet::new();
+    let mut search_stack = requires.clone();
+    while let Some(element) = search_stack.pop() {
+        let dependency_name = find_dependency_name(element);
+        if let Some(dependency_name) = dependency_name {
+            dependency_names.insert(dependency_name.clone());
+
+            // Climb up to look for any more dependencies
+            // TODO: we should structure this so that we can topo-sort
+            let dependency_stmt = assignment_map.get(&dependency_name);
+            if let Some(dependency_stmt) = dependency_stmt {
+                search_stack.push(dependency_stmt);
+            }
+        }
+    }
+
     // From the remaining statements, find all the assignments which a require is dependent on
-    let dependency_names = find_dependency_names(requires.as_slice());
     let (dependencies, remainders) = remainders
         .iter()
         .partition(|stmt| is_dependency(&stmt.0, &dependency_names));
