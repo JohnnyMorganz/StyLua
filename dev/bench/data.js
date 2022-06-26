285f5993a0bf900585e08566abfa37e9489487bc
github-action-benchmark <github@users.noreply.github.com>
2022-06-26 18:31:11 +0000

    add Rust Benchmark (cargo) benchmark result for 4e9f45432247d9635f5ba0c108fd6f04f5551636

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656267897463,
+  "lastUpdate": 1656268271347,
   "repoUrl": "https://github.com/JohnnyMorganz/StyLua",
   "entries": {
     "Rust Benchmark": [
@@ -41,6 +41,48 @@ window.BENCHMARK_DATA = {
             "name": "format nested_tables.lua",
             "value": 48883980,
             "range": "± 507335",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "johnnymorganz@outlook.com",
+            "name": "JohnnyMorganz",
+            "username": "JohnnyMorganz"
+          },
+          "committer": {
+            "email": "johnnymorganz@outlook.com",
+            "name": "JohnnyMorganz",
+            "username": "JohnnyMorganz"
+          },
+          "distinct": true,
+          "id": "4e9f45432247d9635f5ba0c108fd6f04f5551636",
+          "message": "Minor readme cleanup",
+          "timestamp": "2022-06-26T19:24:30+01:00",
+          "tree_id": "87b6426e7bab4866d42c8ccf16d6db7272f04283",
+          "url": "https://github.com/JohnnyMorganz/StyLua/commit/4e9f45432247d9635f5ba0c108fd6f04f5551636"
+        },
+        "date": 1656268270692,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "format date.lua",
+            "value": 60142578,
+            "range": "± 300636",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "format docgen.lua",
+            "value": 2736650339,
+            "range": "± 2548463",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "format nested_tables.lua",
+            "value": 52392872,
+            "range": "± 215979",
             "unit": "ns/iter"
           }
         ]
