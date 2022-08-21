78ec64f82a764c43fc74cfa79e5860553d83e45e
JohnnyMorganz <johnnymorganz@outlook.com>
2022-08-21 15:14:11 +0100

    Fix table field shape calculation (#552)
    * Add test case
    
    * Fix table shape computation
    
    * Changelog
    
    * Snapshots

---
+++ tests/inputs/table-8.lua
@@ -0,0 +1,4 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/551
+local test = {
+	{ "http://example.com/b//c//d;p?q#blarg", "http://u:p@h.com/p/a/t/h?s#hash2", "http://u:p@h.com/p/a/t/h?s#hash2" },
+}
