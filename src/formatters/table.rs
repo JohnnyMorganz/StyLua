14796823b8d7d347d2b5d691d954a1fc552ed1b0
JohnnyMorganz <johnnymorganz@outlook.com>
2022-08-21 15:01:49 +0100

    Fix table shape computation

--- src/formatters/table.rs
+++ src/formatters/table.rs
@@ -20,7 +20,7 @@ use full_moon::{
         Expression, Field, TableConstructor, Value,
     },
     node::Node,
-    tokenizer::{Token, TokenKind, TokenReference, TokenType},
+    tokenizer::{Token, TokenReference, TokenType},
 };
 
 /// Used to provide information about the table
@@ -477,15 +477,15 @@ pub fn format_table_constructor(
                 let additional_shape = match (
                     start_brace
                         .trailing_trivia()
-                        .any(|x| x.token_kind() == TokenKind::Whitespace),
+                        .any(trivia_util::trivia_is_whitespace),
                     // A space will be present on the end of the last field, not the start of the end brace
                     match (last_field.value(), last_field.punctuation()) {
                         (_, Some(token)) => token
                             .trailing_trivia()
-                            .any(|x| x.token_kind() == TokenKind::Whitespace),
+                            .any(trivia_util::trivia_is_whitespace),
                         (field, None) => table_field_trailing_trivia(field)
                             .iter()
-                            .any(|x| x.token_kind() == TokenKind::Whitespace),
+                            .any(trivia_util::trivia_is_whitespace),
                     },
                 ) {
                     (true, true) => 0,
@@ -493,11 +493,8 @@ pub fn format_table_constructor(
                     (false, false) => 2,
                 };
 
-                let singleline_shape = shape
-                    + (braces_range.1 - braces_range.0)
-                    + additional_shape
-                    + BRACE_LEN
-                    + BRACE_LEN;
+                let singleline_shape =
+                    shape + (braces_range.1 - braces_range.0) + additional_shape + BRACE_LEN; // End brace is not included in braces range
 
                 match singleline_shape.over_budget() {
                     true => TableType::MultiLine,
