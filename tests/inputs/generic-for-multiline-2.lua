2d4bd31787a8e32d285f61ace45a6e77c1983b7b
JohnnyMorganz <johnnymorganz@outlook.com>
2022-09-19 19:57:00 +0100

    Fix mistransformation of generic for comments (#580)
    * Fix incorrect token used in multiline generic for
    
    * Add test case
    
    * Fix check for comments in generic_for expr
    
    * Fix trivia location for multiline generic for
    
    * Add snapshot
    
    * Update changelog

---
+++ tests/inputs/generic-for-multiline-2.lua
@@ -0,0 +1,19 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/579
+for _, item in
+	-- comment
+	call()
+do
+end
+
+
+for _, item in -- comment
+	call()
+do
+end
+
+
+for _, item in           -- comment
+
+	call()
+do
+end
