c4c9f363ab40575e787fe548dcdae1803ca2a5b2
JohnnyMorganz <johnnymorganz@outlook.com>
2022-09-19 20:17:33 +0100

    Take into account token width for if expression formatting (#583)
    * Add test case
    
    * Take into account token width for if expression
    
    * Update changelog
    
    * Ignore trivia in width calculation
    
    * Add snapshot

---
+++ tests/inputs-luau/if-expression-5.lua
@@ -0,0 +1,8 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/582
+do
+	do
+		local defaultValue = if aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+			then aaaaaaaaaaaa(bbbbbbbbbb(cccccccccccccccccccccccccccccccccccc :: string), type_ :: dddddddddddddddddddddd)
+			else nil
+	end
+end
