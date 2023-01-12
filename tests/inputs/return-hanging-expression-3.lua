ccc77290baeeadd2359760bc83c1bfaaea7aaac4
JohnnyMorganz <johnnymorganz@outlook.com>
2023-01-12 16:12:30 +0000

    Add another test case

---
+++ tests/inputs/return-hanging-expression-3.lua
@@ -0,0 +1,55 @@
+function foo()
+	return (
+		long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+	), (
+		long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+	)
+end
+
+function foo()
+	return foo and bar or baz, (
+		long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+	)
+end
+
+function foo()
+	return not (
+		long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+		+ long_variable_name
+	)
+end
