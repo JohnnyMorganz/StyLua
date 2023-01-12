5b6caf7480257fe94a2fab504c32084f69c26c8d
JohnnyMorganz <johnnymorganz@outlook.com>
2023-01-12 13:37:22 +0000

    Add test case

---
+++ tests/inputs/return-newline-after-token.lua
@@ -0,0 +1,16 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/605
+function foo()
+	return
+		delta.tag == Band or
+		delta.tag == Drum or
+		delta.tag == Bass
+end
+
+function foo()
+	return
+		delta.tag == Band or
+		delta.tag == Drum or
+		delta.tag == Bass or
+		delta.tag == Lol or
+		delta.tag == Hello
+end
