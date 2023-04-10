05bacfd92e364c57efead3e386db66acfdf973f2
JohnnyMorganz <johnnymorganz@outlook.com>
2023-04-10 19:00:47 +0100

    Downgrade wasm-pack version

--- wasm/build-wasm.sh
+++ wasm/build-wasm.sh
@@ -1,5 +1,5 @@
 # TODO: Ensure that version is up to date
 cp README.md wasm/
 cp LICENSE.md wasm/
-npx wasm-pack build --target nodejs --out-dir wasm/stylua.node -- --features lua52,lua53,lua54,luau
-npx wasm-pack build --target bundler --out-dir wasm/stylua.bundler -- --features lua52,lua53,lua54,luau
+npx wasm-pack@0.10.3 build --target nodejs --out-dir wasm/stylua.node -- --features lua52,lua53,lua54,luau
+npx wasm-pack@0.10.3 build --target bundler --out-dir wasm/stylua.bundler -- --features lua52,lua53,lua54,luau
