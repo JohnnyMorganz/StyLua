5c2577f238e162589287d63f55dc0ad4d5b585c2
JohnnyMorganz <johnnymorganz@outlook.com>
2023-04-10 18:59:51 +0100

    Update actions-rs and wasm-pack build

--- wasm/build-wasm.sh
+++ wasm/build-wasm.sh
@@ -1,5 +1,4 @@
 # TODO: Ensure that version is up to date
-npm install -g wasm-pack
 cp README.md wasm/
 cp LICENSE.md wasm/
 npx wasm-pack build --target nodejs --out-dir wasm/stylua.node -- --features lua52,lua53,lua54,luau
