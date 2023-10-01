582bdf37431d00732adaa0076881ef8b219307e4
github-action-benchmark <github@users.noreply.github.com>
2023-10-01 14:39:25 +0000

    add Rust Benchmark (cargo) benchmark result for 8d43b932528fa527fc0603cf1c918f3edf908e19

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696171102464,
+  "lastUpdate": 1696171165803,
   "repoUrl": "https://github.com/JohnnyMorganz/StyLua",
   "entries": {
     "Rust Benchmark": [
@@ -8693,6 +8693,48 @@ window.BENCHMARK_DATA = {
             "name": "format nested_tables.lua",
             "value": 28058659,
             "range": "± 161942",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d43b932528fa527fc0603cf1c918f3edf908e19",
+          "message": "Bump vscode-test from 1.5.1 to 1.6.1 in /stylua-vscode (#797)\n\nBumps [vscode-test](https://github.com/Microsoft/vscode-test) from 1.5.1 to 1.6.1.\r\n- [Changelog](https://github.com/microsoft/vscode-test/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/Microsoft/vscode-test/compare/v1.5.1...v1.6.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: vscode-test\r\n  dependency-type: direct:development\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-10-01T16:34:24+02:00",
+          "tree_id": "33362b3a5aca6640a31ede2e4c187b13551f40ce",
+          "url": "https://github.com/JohnnyMorganz/StyLua/commit/8d43b932528fa527fc0603cf1c918f3edf908e19"
+        },
+        "date": 1696171165260,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "format date.lua",
+            "value": 54975907,
+            "range": "± 454339",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "format docgen.lua",
+            "value": 377967159,
+            "range": "± 1561763",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "format nested_tables.lua",
+            "value": 28131062,
+            "range": "± 153107",
             "unit": "ns/iter"
           }
         ]
