fcf04c0b8bc8006f57631a87874a3a9c2b3a278a
JohnnyMorganz <johnnymorganz@outlook.com>
2023-12-23 12:45:25 +0100

    Add test case

---
+++ tests/inputs/function-definition-multiline-2.lua
@@ -0,0 +1,4 @@
+-- https://github.com/JohnnyMorganz/StyLua/issues/830
+local a_very_long_variable_name_given_that_is_bigger_than_width_upper_limit_but_unfortunately_can_not_be_made_shorter = function()
+	print("Hello")
+end
