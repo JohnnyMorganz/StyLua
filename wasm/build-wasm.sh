373fa77733a8008f6716dbbdcc850bae3886a771
Matthew Toohey <contact@mtoohey.com>
2024-10-26 20:00:53 -0400

    Update wasm-bindgen and wasm-pack

--- wasm/build-wasm.sh
+++ wasm/build-wasm.sh
@@ -1,10 +1,7 @@
 # TODO: Ensure that version is up to date
 cp README.md wasm/
 cp LICENSE.md wasm/
-npx wasm-pack@0.10.3 build --target web --out-dir wasm/stylua.web -- --features lua52,lua53,lua54,luau
-
-# workaround for bundler usage
-echo "export { getImports as __getImports, finalizeInit as __finalizeInit }" >> wasm/stylua.web/stylua_lib.js
+npx wasm-pack@0.13.0 build --target web --out-dir wasm/stylua.web -- --features lua52,lua53,lua54,luau
 
 # bundle for node CommonJS
 npx rollup@4.9.5 wasm/src/stylua_lib_node.cjs --file wasm/stylua_lib.cjs --format cjs
