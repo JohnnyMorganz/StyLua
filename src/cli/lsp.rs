3df934a5b318685297ac60308e0d11923ac0d58b
JohnnyMorganz <johnnymorganz@outlook.com>
2025-09-14 17:02:13 +0200

    Reorganise, handle range formatting

--- src/cli/lsp.rs
+++ src/cli/lsp.rs
@@ -1,12 +1,128 @@
-use lsp_server::{Connection, Message, RequestId, Response};
-use lsp_textdocument::TextDocuments;
+use std::convert::TryInto;
+
+use lsp_server::{Connection, ErrorCode, Message, Response};
+use lsp_textdocument::{FullTextDocument, TextDocuments};
 use lsp_types::{
     request::{Formatting, RangeFormatting, Request},
-    DocumentFormattingParams, OneOf, Position, Range, ServerCapabilities,
-    TextDocumentSyncCapability, TextDocumentSyncKind, TextEdit,
+    DocumentFormattingParams, DocumentRangeFormattingParams, OneOf, Position, Range,
+    ServerCapabilities, TextDocumentSyncCapability, TextDocumentSyncKind, TextEdit, Uri,
 };
+use stylua_lib::{format_code, OutputVerification};
 
 use crate::{config::ConfigResolver, opt};
+
+fn handle_formatting(
+    uri: &Uri,
+    document: &FullTextDocument,
+    range: Option<stylua_lib::Range>,
+    config_resolver: &mut ConfigResolver,
+) -> Option<Vec<TextEdit>> {
+    if document.language_id() != "lua" && document.language_id() != "luau" {
+        return None;
+    }
+
+    let contents = document.get_content(None);
+
+    let config = config_resolver
+        .load_configuration(uri.path().as_str().as_ref())
+        .unwrap_or_default();
+
+    let formatted_contents = format_code(contents, config, range, OutputVerification::None).ok()?;
+
+    let last_line_idx = document.line_count().saturating_sub(1);
+    let last_line_offset = document.offset_at(Position::new(last_line_idx, 0));
+    let last_col = document.content_len() - last_line_offset;
+
+    // TODO: We can be smarter about this in the future, and update only the parts that changed (using output_diff)
+    Some(vec![TextEdit {
+        range: Range {
+            start: Position::new(0, 0),
+            end: Position::new(last_line_idx, last_col),
+        },
+        new_text: formatted_contents,
+    }])
+}
+
+fn handle_request(
+    request: lsp_server::Request,
+    documents: &TextDocuments,
+    config_resolver: &mut ConfigResolver,
+) -> Response {
+    match request.method.as_str() {
+        Formatting::METHOD => {
+            match serde_json::from_value::<DocumentFormattingParams>(request.params) {
+                Ok(params) => {
+                    let Some(document) = documents.get_document(&params.text_document.uri) else {
+                        return Response::new_err(
+                            request.id,
+                            ErrorCode::RequestFailed as i32,
+                            format!(
+                                "no document found for '{}'",
+                                params.text_document.uri.as_str()
+                            ),
+                        );
+                    };
+
+                    match handle_formatting(
+                        &params.text_document.uri,
+                        document,
+                        None,
+                        config_resolver,
+                    ) {
+                        Some(edits) => Response::new_ok(request.id, edits),
+                        None => Response::new_ok(request.id, serde_json::Value::Null),
+                    }
+                }
+                Err(err) => Response::new_err(
+                    request.id,
+                    lsp_server::ErrorCode::RequestFailed as i32,
+                    err.to_string(),
+                ),
+            }
+        }
+        RangeFormatting::METHOD => {
+            match serde_json::from_value::<DocumentRangeFormattingParams>(request.params) {
+                Ok(params) => {
+                    let Some(document) = documents.get_document(&params.text_document.uri) else {
+                        return Response::new_err(
+                            request.id,
+                            1,
+                            format!(
+                                "no document found for '{}'",
+                                params.text_document.uri.as_str()
+                            ),
+                        );
+                    };
+
+                    let range = stylua_lib::Range::from_values(
+                        Some(document.offset_at(params.range.start).try_into().unwrap()),
+                        Some(document.offset_at(params.range.end).try_into().unwrap()),
+                    );
+
+                    match handle_formatting(
+                        &params.text_document.uri,
+                        document,
+                        Some(range),
+                        config_resolver,
+                    ) {
+                        Some(edits) => Response::new_ok(request.id, edits),
+                        None => Response::new_ok(request.id, serde_json::Value::Null),
+                    }
+                }
+                Err(err) => Response::new_err(
+                    request.id,
+                    lsp_server::ErrorCode::RequestFailed as i32,
+                    err.to_string(),
+                ),
+            }
+        }
+        _ => Response::new_err(
+            request.id,
+            lsp_server::ErrorCode::MethodNotFound as i32,
+            format!("server does not support method '{}'", request.method),
+        ),
+    }
+}
 
 pub fn run(opt: opt::Opt) -> anyhow::Result<()> {
     // Load the configuration
@@ -34,30 +150,8 @@ pub fn run(opt: opt::Opt) -> anyhow::Result<()> {
                     break;
                 }
 
-                match req.method.as_str() {
-                    Formatting::METHOD => {
-                        let result =
-                            match serde_json::from_value::<DocumentFormattingParams>(req.params) {
-                                Ok(params) => {
-                                    let res = handle_formatting(
-                                        req.id.clone(),
-                                        params,
-                                        &mut config_resolver,
-                                        &documents,
-                                    );
-
-                                    res.unwrap_or(Response::new_ok(req.id, serde_json::Value::Null))
-                                }
-                                Err(err) => Response::new_err(req.id, 1, err.to_string()),
-                            };
-
-                        connection.sender.send(Message::Response(result))?;
-                    }
-                    RangeFormatting::METHOD => {
-                        // TODO: Would be cool to support this in the future
-                    }
-                    _ => {}
-                }
+                let response = handle_request(req, &documents, &mut config_resolver);
+                connection.sender.send(Message::Response(response))?
             }
             Message::Response(_) => {}
             Message::Notification(notification) => {
@@ -69,40 +163,4 @@ pub fn run(opt: opt::Opt) -> anyhow::Result<()> {
     io_threads.join()?;
 
     Ok(())
-}
-
-fn handle_formatting(
-    id: RequestId,
-    params: DocumentFormattingParams,
-    config_resolver: &mut ConfigResolver,
-    documents: &TextDocuments,
-) -> Option<Response> {
-    let uri = params.text_document.uri;
-    let path = uri.path().as_str();
-
-    let src = documents.get_document_content(&uri, None)?;
-
-    let config = config_resolver
-        .load_configuration(path.as_ref())
-        .unwrap_or_default();
-
-    let new_text =
-        stylua_lib::format_code(src, config, None, stylua_lib::OutputVerification::None).ok()?;
-
-    if new_text == src {
-        return None;
-    }
-
-    // TODO: We can be smarter about this in the future, and update only the parts that changed
-    let edit = TextEdit {
-        range: Range {
-            start: Position::new(0, 0),
-            end: Position::new(u32::MAX, 0),
-        },
-        new_text,
-    };
-
-    let edits: Vec<TextEdit> = vec![edit];
-
-    Some(Response::new_ok(id, edits))
 }
