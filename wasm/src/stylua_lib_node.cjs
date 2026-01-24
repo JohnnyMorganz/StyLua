becb27276cf378bc38e3759c58221d254d051671
magic-akari <akari.ccino@gmail.com>
2026-01-24 21:10:50 +0800

    Remove WASM build workaround (#1060)
    * Remove wasm workaround
    
    * Add smoketests
    
    ---------
    
    Co-authored-by: JohnnyMorganz <johnnymorganz@outlook.com>

--- wasm/src/stylua_lib_node.cjs
+++
@@ -1,7 +0,0 @@
-export * from "../stylua.web/stylua_lib.js";
-import { initSync } from "../stylua.web/stylua_lib.js";
-
-const path = require("path").join(__dirname, "stylua.web/stylua_lib_bg.wasm");
-const bytes = require("fs").readFileSync(path);
-
-initSync(bytes);
